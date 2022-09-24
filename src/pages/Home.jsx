5704797b042cee43211b22efaad331a74ed50b49
mahannajafi <mahan.nwork@gmail.com>
2022-09-24 16:43:53 +0330

    newsletter added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Categories from '../Components/Categories'
 import Navbar from '../Components/Navbar'
 import Slider from '../Components/Slider'
 import Products from '../Components/Products'
+import Newsletter from '../Components/Newsletter'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Slider/>
       <Categories/>
       <Products/>
+      <Newsletter/>
     </div>
   )
 }
